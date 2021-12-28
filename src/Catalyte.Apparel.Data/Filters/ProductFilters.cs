dadc764801ca50f3888dd0376603a5e6a654dc7b
Chris Gomes <cgomes@catalyte.io>
2021-12-28 13:56:05 -0600

    Add filters for Category, Demographic, and Color

--- src/Catalyte.Apparel.Data/Filters/ProductFilters.cs
+++ src/Catalyte.Apparel.Data/Filters/ProductFilters.cs
@@ -13,5 +13,15 @@ namespace Catalyte.Apparel.Data.Filters
         {
             return products.Where(i => i.Id == productId).AsQueryable();
         }
+
+        public static IQueryable<Product> WhereProductCategoryEquals(this IEnumerable<Product> products, string productCategory)
+        {
+            return products.Where(i => i.Category == productCategory).AsQueryable();
+        }
+
+        public static IQueryable<Product> WhereProductDemographicEquals(this IEnumerable<Product> products, string productDemographic)
+        {
+            return products.Where(i => i.Demographic == productDemographic).AsQueryable();
+        }
     }
 }
