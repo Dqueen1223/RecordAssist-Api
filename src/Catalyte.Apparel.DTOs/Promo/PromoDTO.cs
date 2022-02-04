260576e77b0a4cdd04130271d4c0ab6a2e05a3da
Slate Lucia <slucia@catalyte.io>
2022-02-04 17:33:54 -0500

    wasted a day fiddling with DateTimes. I still want to write one more test, but functionality will be my priority monday

--- src/Catalyte.Apparel.DTOs/Promo/PromoDTO.cs
+++ src/Catalyte.Apparel.DTOs/Promo/PromoDTO.cs
@@ -15,8 +15,8 @@ namespace Catalyte.Apparel.DTOs.Promos
 
         public string Type { get; set; }
 
-        public DateTime StartDate { get; set; }
+        public DateTimeOffset StartDate { get; set; }
 
-        public DateTime EndDate { get; set; }
+        public DateTimeOffset EndDate { get; set; }
     }
 }
--- src/Catalyte.Apparel.Data/Model/Promo.cs
+++ src/Catalyte.Apparel.Data/Model/Promo.cs
@@ -17,8 +17,8 @@ namespace Catalyte.Apparel.Data.Model
 
         public string Type { get; set; }
         
-        public DateTime StartDate { get; set; }
+        public DateTimeOffset StartDate { get; set; }
 
-        public DateTime EndDate { get; set; }
+        public DateTimeOffset EndDate { get; set; }
     }
 }
